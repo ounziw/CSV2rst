db8058493317480b06932d6e354854d10a05a2ee
Fumito Mizuno <ounziw@gmail.com>
2011-10-13 09:56:26 +0900

    choose to split or not

--- csv2rst.php
+++ csv2rst.php
@@ -1,12 +1,5 @@
 <?php
 
-    /*
-     * テーブルの長さを指定する
-     * lengthcheck.php を実行した結果を目安にしてください
-     */
-    $maxlength_array = array(14,18,28,50);
-
-
     $fh_in = fopen('sample.csv','r');
     $fh_out = fopen('sample.rst','w');
     $dataarray = array();
@@ -16,11 +9,29 @@
 
     include_once('makerst.php');
     $makerst = new Makerst($dataarray,$maxlength_array);
+
+    /*
+     * テーブルの長さを指定する
+     * lengthcheck.php を実行した結果を目安にしてください
+     */
+    $maxlength_array = array(14,18,28,50);
+    /*
+     * 折り返し無(一番長いものに合わせる)はこちら
+     */
+    /*
+    include_once('max_length.php');
+    $maxlength = new Max_length($dataarray);
+    $maxlength_array = $maxlength->get_max_length_data();
+     */
+
     
     $out = "";
     $out .= $makerst->lineout();
     $out .= $makerst->outall();
     $out .= $makerst->lineout();
+    /*
+     * 区切りを空白でないものにする場合はこちら
+     */
     /*
     $out .= $makerst->lineout('+');
     $out .= $makerst->outall('|');
